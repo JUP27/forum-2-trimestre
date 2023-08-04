48332aa42c9d6eb51354705c49325734e18e6063
Ana Carolina <kakarolina2006@hotmail.com>
2023-08-04 20:48:10 -0300

    cadastro-login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import Cadastro from './paginas/cadastro/Cadastro';
+import Login from './paginas/login/Login';
+import CreateStyledGlobal from './styledGlobal';
 
 function App() {
   return (
    <>
-   
+   <CreateStyledGlobal/>
+   <Cadastro/>
    </>
   );
 }
